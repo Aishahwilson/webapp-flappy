64420b24e32b9eff568323bad53e48b183a1e2a9
V5809 (Course ID) <v5809@cam.ac.uk>
2015-08-13 10:03:44 +0100

    End of day 3

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 var express = require("express");
 var path = require("path");
-
+var bodyParser = require("body-parser");
+var csv = require("ya-csv");
 var app = express();
 app.use(express.static(path.join(__dirname, "")));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(request, response){
-    response.sendFile(path.join(__dirname, "pages/index.html"));
+    response.sendFile(path.join(__dirname, "pages/game.html"));
 });
 
-app.post('/score', function(request, response){
-    
+app.post("/score", function(request, response){
+    var name = request.body.fullName;
+    var email = request.body.email;
+    var score = request.body.score;
+    var database = csv.createCsvFileWriter("scores.csv", {"flags": "a"});
+    var data = [name, email, score];
+    database.writeRecord(data);
+    database.writeStream.end();
+    response.redirect("/");
+});
+
+app.get("/score", function(request, response) {
+    var reader = csv.createCsvFileReader("scores.csv");
+    reader.setColumnNames(["name", "email", "score"]);
+    var scores = [];
+    reader.addListener("data", function(data) {
+        scores.push(data);
+    });
+    reader.addListener("end", function(){
+        response.send(scores);
+    })
 });
 
 var server = app.listen(8080, function() {
--- js/flappy.js
+++ js/flappy.js
@@ -7,13 +7,52 @@ var stateActions = { preload: preload, create: create, update: update };
 // - renderer (go for Phaser.AUTO)
 // - element where the game will be drawn ('game')
 // - actions on the game state (or null for nothing)
-var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
+
+var width = 790;
+var height = 400;
+var gameSpeed = 200;
+var gameGravity = 450;
+var jumpPower = 200;
+var gapSize = 100
+var gapMargin = 50
+var blockHeight = 50
+
+var game = new Phaser.Game(width, height, Phaser.AUTO, 'game', stateActions);
 
 /*
  * Loads all resources for the game and gives them names.
  */
-function preload() {
+jQuery("#greeting-form").on("submit", function(event_details) {
+    var greeting = "Hello ";
+    var name = $("#fullName").val();
+    var email = $("#email").val();
+    var score = $ ("#score").val();
+    var greeting_message = greeting + name + "<p>" + "(" + email + ")"
+       + "</p>" + "<p>" + "Your score is:" + score + "</p>";
+    $("#greeting-form").hide();
+    $("#greeting").append("<p>" + greeting_message + "</p>");
+    //event_details.preventDefault();
+});
+var score = 0;
+var labelScore;
+var player;
+var pipes = [];
+var pipeInterval;
+var balloons = [];
+var weights = [];
+var started = false;
+var background;
+var splashDisplay;
+var stars;
 
+function preload() {
+    game.load.image("playerImg", "../assets/httyd3");
+    game.load.audio("score", "../assets/point.ogg");
+    game.load.image("backgroundImg", "../assets/cavesez2.jpg");
+    game.load.image("pipe","../assets/Stone_slab");
+    game.load.image("balloon", "../assets/gold_coin");
+    game.load.image("weight", "../assets/red_egg");
+    game.load.image("stars", "../assets/star.png")
 
 }
 
@@ -22,11 +61,270 @@ function preload() {
  */
 function create() {
     // set the background colour of the scene
+    game.stage.setBackgroundColor("#000000");
+    game.add.text(250, 150, "Flappy Dragon!",
+        {font: "50px Highway Gothic", fill: "#FF6600"});
+    game.add.sprite(200, 157, "playerImg");
+    game.add.sprite(560, 157, "playerImg");
+    background = game.add.image(0, 0, "backgroundImg");
+    background.width=790
+    background.height=400
+    /*game.input.onDown.add(clickHandler);
+    game.input.keyboard
+        .addKey(Phaser.Keyboard.SPACEBAR)
+        .onDown.add(spaceHandler);
+    //alert(score);*/
+    labelScore = game.add.text(20, 20, "0",
+        {font: "50px Highway Gothic", fill: "#FF6600"});
+    player = game.add.sprite(100, 200, "playerImg");
+    player.width=50;
+    player.height=45;
+    player.anchor.setTo(0.5, 0.5);
+    game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+        .onDown.add(moveRight);
+    game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+        .onDown.add(moveLeft);
+    game.input.keyboard.addKey(Phaser.Keyboard.UP)
+        .onDown.add(moveUp);
+    game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
+        .onDown.add(moveDown);
+    //for(var count=0; count<8; count++){
+        //game.add.sprite(150, 50 * count, "pipe");
+    //}
+    //generatePipe();
+
+   /* game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.enable(player);
+    player.body.velocity.x = 0;
+    player.body.velocity.y = -300;
+    player.body.gravity.y = gameGravity;
+    game.input.keyboard
+        .addKey(Phaser.Keyboard.SPACEBAR)
+        .onDown.add(playerJump);
+    pipeInterval = 1.80;
+    game.time.events
+        .loop(pipeInterval * Phaser.Timer.SECOND,
+       generate);*/
+    game.input.keyboard
+        .addKey(Phaser.Keyboard.ENTER)
+        .onDown.add(start);
+    splashDisplay = game.add.text(250, 150, "Press ENTER to start");
+
+
+}
+function clickHandler(event){
+    //alert("The position is: " + event.x + "," + event.y);
+    game.add.sprite(event.x, event.y, "playerImg")
+}
+function spaceHandler(){
+    game.sound.play("score")
+}
+function changeScore(){
+    score = score + 1;
+    labelScore.setText(score.toString());
+
+}
+function moveRight(){
+    player.x = player.x + 10;
+}
+function moveLeft(){
+    player.x = player.x - 10;
+}
+function moveUp() {
+    player.y = player.y - 10;
+}
+function moveDown() {
+    player.y = player.y + 10;
+}
+
+function generatePipe() {
+    /*for (var count1 = 0; count1 < 6; count1 = count1 + 1) {
+        var gapStart = game.rnd.integerInRange(1, 5);
+        for (var count = 0; count < 8; count = count + 1) {
+            if (count != gapStart && count != gapStart + 1) {
+                addPipeBlock(count1 * 150, count*50);
+            }
+        }
+    }
+    var gap = game.rnd.integerInRange(1 ,5);
+    for (var count = 0; count < 8; count++) {
+        if (count != gap && count != gap+1) {
+            addPipeBlock(750, count * 50);
+        }
+    }*/
+    var gapStart = game.rnd.integerInRange(gapMargin, height - gapSize - gapMargin);
+    for(var y = gapStart; y > 0; y -= blockHeight ){
+        addPipeBlock(width,y - blockHeight);
+    }
+    for(var y = gapStart + gapSize; y < height; y += blockHeight) {
+        addPipeBlock(width, y);
+    }
+
+       changeScore();
+}
+
+function addPipeBlock(x, y) {
+// create a new pipe block
+    var pipeBlock = game.add.sprite(x,y,"pipe");
+// insert it in the 'pipes' array
+    pipes.push(pipeBlock);
+    game.physics.arcade.enable(pipeBlock);
+    pipeBlock.body.velocity.x = -gameSpeed;
+}
+function playerJump() {
+    player.body.velocity.y = -jumpPower;
 }
 
 /*
  * This function updates the scene. It is called for every new frame.
  */
 function update() {
+    if (started) {
+        for (var index = 0; index < pipes.length; index++) {
+            game.physics.arcade
+                .overlap(player,
+                pipes[index],
+                gameOver);
+        }
+        if (player.body.y < 0 || player.body.y > 400) {
+            gameOver();
+        }
+        // player.rotation += 1;
+        player.rotation = Math.atan(player.body.velocity.y / gameSpeed);
+
+        for (var i = balloons.length - 1; i >= 0; i--) {
+            game.physics.arcade.overlap(player, balloons[i], function () {
+
+                changeGravity(-50);
+                balloons[i].destroy();
+                balloons.splice(i, 1);
+
+            });
+        }
+        for (var i = weights.length - 1; i >= 0; i--) {
+            game.physics.arcade.overlap(player, weights[i], function () {
+
+                changeGravity(50);
+                weights[i].destroy();
+                weights.splice(i, 1);
+
+            }, gameOver);
+        }
+
+        // checkBonus(balloons, -50);
+        // checkBonus(weights, 50);
+
+    }
+}
+
+/*function checkBonus(bonusArray, bonusEffect){
+    for(var i=bonusArray.length - 1; i>=0; i--){
+        game.physics.arcade.overlap(player,bonusArray[i], function(){
+            changeGravity(bonusEffect);
+            bonusArray[i].destroy();
+            bonusArray.splice(i,1);
+        });
+    }
+}*/
+
+function gameOver(){
+
+    //game.add.text(0, 0, "Game Over", {font: "50px Highway Gothic", fill: "#FFFFFF"});
+   //game.destroy();
+    // game.paused(true);
+    game.paused = true;
+    $("#score").val(score.toString());
+    $("#greeting").show();
+    score = 0;
+    //$("#greeting").hide();
+    //game.state.restart();
+    //location.reload();
+    gameGravity = 200;
+
+}
+
+$.get("/score", function(scores){
+    //console.log("Data: ",scores);
+    //for (var i = 0; i < scores.length; i++) {
+        //$("#scoreBoard").append(
+           // "<li>" +
+           // scores[i].name + ": " + scores[i].score +
+            //"</li>");
+    scores.sort(function (scoreA, scoreB){
+        var difference = scoreB.score - scoreA.score;
+        return difference;
+    });
+//    for (var i = 0; i < scores.length; i++) {
+    for (var cat = 0; cat < 5; cat++) {
+        $("#scoreBoard").append(
+            "<li>" +
+            scores[cat].name + ": " + scores[cat].score +
+            "</li>");
+    }
+    if(isEmpty(fullName)) {
+        response.send("Please make sure you enter your name.");
+    }
+});
+
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
+
+function changeGravity(g) {
+    gameGravity += g;
+    player.body.gravity.y = gameGravity;
+}
+
+function generateBalloon(){
+    var bonus = game.add.sprite(width, height, "balloon");
+    balloons.push(bonus);
+    game.physics.arcade.enable(bonus);
+    bonus.body.velocity.x = -200;
+    bonus.body.velocity.y = -game.rnd.integerInRange(60,100);
+}
+
+function generateWeight(){
+    var bomb = game.add.sprite(width, 0, "weight");
+    weights.push(bomb);
+    game.physics.arcade.enable(bomb);
+    bomb.body.velocity.x = -200;
+    bomb.body.velocity.y = game.rnd.integerInRange(60,100);
+}
+
+function generate() {
+    var diceRoll = game.rnd.integerInRange(1, 10);
+    if(diceRoll==1) {
+        generateBalloon();
+    } else if(diceRoll==2) {
+        generateWeight();
+    } else {
+        generatePipe();
+    }
+}
+/*var arr = ["zero", "one", "two", "three", "four"];
+for(var i = 0; i < arr.length; i++){
+    console.log(arr + " " + i + "th is " + arr[i]);
+    arr.splice(i, 1);
+}*/
+
+function start(){
+    started = true
+    game.input.onDown.add(clickHandler);
+    game.input.keyboard
+        .addKey(Phaser.Keyboard.SPACEBAR)
+        .onDown.add(spaceHandler);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.enable(player);
+    player.body.velocity.x = 0;
+    player.body.velocity.y = -300;
+    player.body.gravity.y = gameGravity;
+    game.input.keyboard
+        .addKey(Phaser.Keyboard.SPACEBAR)
+        .onDown.add(playerJump);
+    pipeInterval = 1.80;
+    game.time.events
+        .loop(pipeInterval * Phaser.Timer.SECOND,
+        generate);
+    splashDisplay.destroy();
 
 }
